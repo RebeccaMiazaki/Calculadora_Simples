05e3dc57a9d0e80e2ef3ca0c11d6f65eec35f6b6
thellzardi12 <130668477+thellzardi12@users.noreply.github.com>
2023-10-31 13:55:32 -0300

    - Commit Inicial Rebecca 2

--- main.py
+++ main.py
@@ -1,51 +1,49 @@
 from Calculadora import soma
 from Calculadora import sub
 
-#Implementação da Calculadora Simples
+#Implementação da Calculadora simples
 
 while True:
     #Apresentação
-    print('\n\t\t -- Calculadora Simples -- \t\t')
+    print('\n\t\t\t --- Calculadora Simples -- \n')
 
     #Menu
-    print('1. Soma')
-    print('2. Subtração')
-    print('3. Sair')
+    print('1.Soma')
+    print('2.Subtração')
+    print('3.Sair')
 
-    #Ler a opção de escolha dpo usuário
-    op = int(input('\n\tOpção: '))
-
-    if op ==1:
+    #Ler a opção de escolha do usuário
+    op = int(input('\n Opção:'))
+    if op == 1 :
         print('\nSoma\n')
-
         #Entrada
-        v1 = int(input('Informe o valor 1: '))
-        v2 = int(input('Informe o valor 2: '))
+        v1= int(input('Informe o valor1:'))
+        v2= int(input('Informe o valor2:'))
 
         #Processamento
-        total = soma(v1,v2)
+        total=soma(v1,v2)
 
         #Saída
-        print(f'\n{v1} + {v2} = {total}\n')
+        print(f'\n n{v1} + n {v2} = {total} \n')
 
-    elif op ==2:
-        print('\nSubtração\n')
+    elif op == 2 :
+        print('\n Subtração\n')
 
         #Entrada
-        v1 = int(input('Informe o valor 1: '))
-        v2 = int(input('Informe o valor 2: '))
+        b1=int(input('digite um numero b1:'))
+        b2=int(input('digite segundo numero b2:'))
 
         #Processamento
-        total = sub(v1, v2)
+        total1=sub(b1,b2)
 
         #Saída
-        print(f'\n{v1} + {v2} = {total}\n')
+        print('\n n{b1} - {b2} = {total1} \n')
 
-    elif op ==3:
+    elif op == 3 :
         #Sair do sistema
-        print('Forte Abraço!')
+        print('Forte Abraço')
         break
 
     else:
         #Tratamento de exceção
-        print(f'\nOpção {op} incorreta!\n')
+        print(f'\n Opção {op} incorreta\n')
